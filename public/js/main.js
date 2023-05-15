3691017ff0495dbb4f61278b9bdba131129c9217
Quốc Zương <quocvuongle.ct@gmail.com>
2023-05-15 17:36:17 +0700

    third commit

--- public/js/main.js
+++ public/js/main.js
@@ -16,18 +16,31 @@ const dragging = (e) => {
   item.scrollLeft += dragStartPos - dragEndPos; // scroll the item element horizontally based on the difference between the start and end drag positions
   dragStartPos = dragEndPos; // update dragStartPos to the current dragEndPos for the next dragging event
   updateDots(); // update the dots based on the current scroll position
+
+  // Check if reached the end and scroll back to the first item
+  const items = document.querySelectorAll(".item .card");
+  const itemWidth = items[0].getBoundingClientRect().width;
+  const lastItemIndex = items.length - 1;
+  const lastItemOffset = lastItemIndex * itemWidth;
+  const scrollEndOffset = item.scrollWidth - item.offsetWidth;http://127.0.0.1:5500/public/images/products/Kilian-Angels-Share-600x600.png
+
+  if (item.scrollLeft >= scrollEndOffset && dragStartPos < dragEndPos) {
+    item.scrollLeft = 0;
+  } else if (item.scrollLeft === 0 && dragStartPos > dragEndPos) {
+    item.scrollLeft = lastItemOffset - item.offsetWidth;
+  }
 };
 
 const dragEnd = () => {
   isDragStart = false;
 };
 
-const updateDots = () => {
+const updateDots = () => { // function to add effect for dots
   const items = document.querySelectorAll(".item .card");
   const itemWidth = items[0].getBoundingClientRect().width;
-  const scrollLeft = item.scrollLeft;
+  const scrollLeft = item.scrollLeft; //return the value when scroll this item
   const currentItem = Math.round(scrollLeft / itemWidth);
-  const dotsArray = Array.from(dots.children);
+  const dotsArray = Array.from(dots.children); // create new array that contain all the child of dots element
   dotsArray.forEach((dot, index) => {
     if (index === currentItem) {
       dot.classList.add("active");
@@ -38,9 +51,38 @@ const updateDots = () => {
 };
 
 const handleDotClick = (e) => {
-  const dotIndex = Array.from(dots.children).indexOf(e.target); // get the index of the clicked dot element
-  item.scrollLeft = item.offsetWidth * dotIndex; // set the scroll position of the item element to show the card corresponding to the clicked dot
+  const dotsArray = Array.from(dots.children);
+  const dotIndex = dotsArray.indexOf(e.target); // get the index of the clicked dot element
+  const items = document.querySelectorAll(".item .card");
+  const itemWidth = items[0].getBoundingClientRect().width; // get the width of the first item
+  const targetItem = dotIndex;
+
+  item.classList.add("scroll-animation");
+  item.scrollLeft = targetItem * itemWidth;
+
+  dotsArray.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+
+  dotsArray[dotIndex].classList.add("active");
+
+  const currentItem = Math.round(item.scrollLeft / itemWidth);
+  
+  if (currentItem < targetItem) {
+    item.scrollLeft = targetItem * itemWidth;
+  } else if (currentItem > targetItem + 1) {
+    item.scrollLeft = (targetItem + 1) * itemWidth;
+  }
+  
 };
+
+dots.querySelectorAll(".dot").forEach((dot) => {
+  dot.addEventListener("click", handleDotClick);
+});
+
+item.addEventListener("transitionend", () => {
+  item.classList.remove("scroll-animation");
+});
 
 item.addEventListener("mousedown", dragStart);
 item.addEventListener("mousemove", dragging);
@@ -50,16 +92,16 @@ item.addEventListener("touchstart", dragStart);
 item.addEventListener("touchmove", dragging);
 item.addEventListener("touchend", dragEnd);
 
-dots.querySelectorAll(".dot").forEach((dot) => {
-  dot.addEventListener("click", handleDotClick);
-});
+
 
 // add dots
 const items = document.querySelectorAll(".item .card");
-for (let i = 0; i < items.length; i++) {
+const dotToGenerate = Math.ceil(items.length / 5); // one dot for 5 items
+for (let i = 0; i < dotToGenerate; i++) {
   const dot = document.createElement("span"); // create a new span element for each card
   dot.classList.add("dot"); // add the dot class to the span element
   dots.appendChild(dot); // append the span element to the dots container element
+  dot.addEventListener("click", handleDotClick);
 }
 
 // update dots on load
