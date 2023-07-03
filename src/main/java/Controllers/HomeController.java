d44c1a32576f57bc03e735dc951bd00bca1e5831
Carl-Johnsons <nguyenletaiduc2709@gmail.com>
2023-07-03 16:16:05 +0700

    Add txt to admin, client

--- src/main/java/Controllers/HomeController.java
+++ src/main/java/Controllers/HomeController.java
@@ -11,16 +11,6 @@ import jakarta.servlet.http.HttpServletResponse;
 public class HomeController extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
      * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
