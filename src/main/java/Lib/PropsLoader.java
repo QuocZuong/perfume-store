b19251f87f81a3481fa52dc87c6e57cc2216e9d7
PhenChua <phenchua29@gmail.com>
2023-09-25 23:24:02 +0700

    Update DataManager, now it will fetch DB's information from a file instead.

---
+++ src/main/java/Lib/PropsLoader.java
@@ -0,0 +1,53 @@
+package Lib;
+
+import java.io.FileInputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+public class PropsLoader {
+
+	private Properties props;
+	private boolean noData;
+
+	public PropsLoader() {
+		props = new Properties();
+
+		try {
+			noData = false;
+
+			String filePath = String.format("%s/%s", System.getProperty("user.dir").replace("\\", "/"), "db.properties");
+			FileInputStream in = new FileInputStream(filePath);
+
+			props.load(in);
+			in.close();
+		} catch (Exception e) {
+			noData = true;
+			e.printStackTrace();
+		}
+	}
+
+	public String getDriver() {
+		return noData ? "" : props.getProperty("DRIVER");
+	}
+
+	public String getServerName() {
+		return noData ? "" : props.getProperty("SERVER_NAME");
+	}
+
+	public String getPort() {
+		return noData ? "" : props.getProperty("PORT");
+	}
+
+	public String getDatabaseName() {
+		return noData ? "" : props.getProperty("DATABASE_NAME");
+	}
+
+	public String getUser() {
+		return noData ? "" : props.getProperty("USER");
+	}
+
+	public String getPassword() {
+		return noData ? "" : props.getProperty("PASSWORD");
+	}
+}
