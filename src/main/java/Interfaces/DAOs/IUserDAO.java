6c33bdb185913ac3405cc91986e2a8f5a0817ffb
PhenChua <phenchua29@gmail.com>
2023-10-07 21:57:44 +0700

    Update all the methods' comments

--- src/main/java/Interfaces/DAOs/IUserDAO.java
+++ src/main/java/Interfaces/DAOs/IUserDAO.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Interfaces.DAOs;
 
 import Models.User;
 import java.sql.Date;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,71 +10,214 @@ import java.util.List;
  */
 public interface IUserDAO {
 
+    /** The name of the table in the database. */
     public final String TABLE_NAME = "User";
-    public final String USER_ID = "User_ID";
+    /** The user's Id column name. */
+    public final String USER_Id = "User_Id";
+    /** The user's name column name. */
     public final String USER_NAME = "User_Name";
+    /** The user's username column name. */
     public final String USER_USERNAME = "User_Username";
+    /** The user's password column name. */
     public final String USER_PASSWORD = "User_Password";
+    /** The user's PhoneNumber number column name. */
     public final String USER_EMAIL = "User_Email";
+    /** The user's PhoneNumber number column name. */
     public final String USER_ACTIVE = "User_Active";
+    /** The user's PhoneNumber number column name. */
     public final String USER_TYPE = "User_Type";
 
+    /**
+     * The login type that used in
+     * {@link IUserDAO#getUser(String loginString, String password, loginType Type)}.
+     */
     public enum loginType {
         Email,
         Username
     }
 
-    public ArrayList<User> getAll();
+    /**
+     * Get all {@link User}s in database.
+     *
+     * @return An {@code ArrayList} containing all {@code User} in database.
+     */
+    public List<User> getAll();
 
+    /**
+     * Gets an {@link User} from the database.
+     *
+     * @param username A {@code String} that represents the username of the
+     *                 {@code User} to be retrieved.
+     * @return A {@code User} object containing the user's information, or
+     *         {@code null} if an error occurs.
+     */
     public User getUser(String username);
 
+    /**
+     * Gets a {@link User} from the database.
+     *
+     * @param Id The Id of the user to be retrieved.
+     * @return A {@code User} object containing the user's information.
+     *         {@code null} if an error occurs or the user is not found.
+     */
+    public User getUser(int Id);
+
+    /**
+     * Gets a {@link User} from the database.
+     *
+     * @param email A {@code String} that represents the email of the
+     *              {@code User} to be retrieved.
+     * @return A {@code User} object containing the user's information.
+     *         {@code null} if an error occurs or the user is not found.
+     */
     public User getUserByEmail(String email);
 
+    /**
+     * Gets a {@link User} from the database.
+     * 
+     * @param loginString A {@code String} that represents the {@code User}'s
+     *                    username or email.
+     * @param password    A {@code String} that represents the password of the
+     *                    {@code User} to be retrieved.
+     * @param Type        A {@code loginType} that represents the login type, as
+     *                    defiened in {@link loginType}.
+     * @return A {@code User} object containing the user's information if the login
+     *         is successful, {@code null} otherwise.
+     */
     public User getUser(String loginString, String password, loginType Type);
 
+    /**
+     * Search for a list of {@link User}s in the database that match the provIded
+     * requirements.
+     * 
+     * @param search The search string.
+     * @return A {@code List} of {@code User}s that match the search string.
+     */
     public List<User> searchUser(String search);
 
+    /**
+     * Get the filtered list from the {@link User} list, according to its page.
+     * 
+     * @param users The {@code User} list to be filtered.
+     * @param page  The page number.
+     * @return A {@link List} of {@code User}s that match the page number.
+     */
     public List<User> pagingUser(List<User> users, int page);
 
     /**
+     * Updates a {@link User}'s information in the database.
      *
-     * @param updateUser
-     * @return the affected rows of executeUpdate()
+     * @param updateUser The {@code User} to be updated.
+     * @return {@code 0} if the {@code User}'s information is updated sucessfully.
+     *         {@code 1} if user
+     *         cannot be updated, or {@code 2} if the {@code User} doesn't exist.
      */
     public int updateUser(User updateUser);
 
-    public int checkout(Integer customerId, Date Date, String address, String phoneNumber, String note, Integer total);
+    /**
+     * This function hanlde the checkout process for a customer.
+     * 
+     * @param customerId  An {@code integer} representing the unique Identifier of
+     *                    the customer.
+     * @param Date        The {@link Date} parameter is used to specify the date of
+     *                    the checkout.
+     * @param address     A {@code String} that represents the customer's address.
+     * @param PhoneNumber A {@code String} that represents the customer's
+     *                    PhoneNumber
+     *                    number.
+     * @param note        A {@code String} that can be used to add any additional
+     *                    information
+     *                    or special instructions related to the checkout process.
+     * @param total       An {@code integer} represents the total amount of the
+     *                    checkout.
+     * @return {@code true} if the checkout process is successful, {@code fasle}
+     *         otherwise.
+     */
+    public boolean checkout(Integer customerId, Date Date, String address, String PhoneNumber, String note,
+            Integer total);
 
     /**
-     *
-     * @param user
-     * @return the affected rows of executeUpdate()
+     * Activate a {@link User} in the database.
+     * 
+     * @param user The {@code User} to be activated.
+     * @return {@code true} if the user is activated, {@code false} otherwise.
      */
-    public int restoreUser(User user);
+    public boolean restoreUser(User user);
 
-    public int disableUser(User user);
+    /**
+     * Deactivate a {@link User} in the database.
+     * 
+     * @param user The {@code User} to be deactivated.
+     * @return {@code true} if the user is deactivated, {@code false} otherwise.
+     */
+    public boolean disableUser(User user);
 
     /**
      * Check all duplication. If Email is duplication throw
      * EmailDuplicationException and so on, ...
      *
-     * @param user
-     * @return {@code true} if it has a duplication otherwise
+     * @param user The {@code User} to be checked.
+     * @return {@code true} if it has a duplication, {@code false} otherwise.
      */
     public boolean checkDuplication(User user);
 
+    /**
+     * Check if the username is exist or not.
+     * 
+     * @param username The username to be checked.
+     * @return {@code true} if the username is exist, {@code false} otherwise.
+     */
     public boolean isExistUsername(String username);
 
-    public boolean isExistUsernameExceptItself(String username, int ID);
+    /**
+     * Check if the username is exist or not, except itself.
+     * 
+     * @param username The username to be checked.
+     * @param Id       The Id of the user to be excluded.
+     * @return {@code true} if the username is exist, {@code false} otherwise.
+     */
+    public boolean isExistUsernameExceptItself(String username, int Id);
 
-    public boolean isExistId(int ID);
+    /**
+     * Check if the Id is exist or not.
+     * 
+     * @param Id The Id to be checked.
+     * @return {@code true} if the Id is exist, {@code false} otherwise.
+     */
+    public boolean isExistId(int Id);
 
-    public boolean isExistPhoneExceptItself(String phone, int ID);
+    /**
+     * Check if the phone number is exist or not, except itself.
+     * 
+     * @param PhoneNumber The phone number to be checked.
+     * @param Id          The Id of the user to be excluded.
+     * @return {@code true} if the phone number is exist, {@code false} otherwise.
+     */
+    public boolean isExistPhoneNumberExceptItself(String PhoneNumber, int Id);
 
-    public boolean isExistPhone(String phone);
+    /**
+     * Check if the phone number is exist or not.
+     * 
+     * @param PhoneNumber The phone number to be checked.
+     * @return {@code true} if the phone number is exist, {@code false} otherwise.
+     */
+    public boolean isExistPhoneNumber(String PhoneNumber);
 
+    /**
+     * Check if the email is exist or not.
+     * 
+     * @param email The email to be checked.
+     * @return {@code true} if the email is exist, {@code false} otherwise.
+     */
     public boolean isExistEmail(String email);
 
-    public boolean isExistEmailExceptItself(String email, int ID);
+    /**
+     * Check if the email is exist or not, except itself.
+     * 
+     * @param email The email to be checked.
+     * @param Id    The Id of the user to be excluded.
+     * @return {@code true} if the email is exist, {@code false} otherwise.
+     */
+    public boolean isExistEmailExceptItself(String email, int Id);
 
 }
