77bd1b7d36315b56049e99e16d0d6cd0a4c65a00
PhenChua <phenchua29@gmail.com>
2023-10-08 15:36:49 +0700

    Remove unneccesary comments

--- src/main/java/DAOs/VoucherDAO.java
+++ src/main/java/DAOs/VoucherDAO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAOs;
 
 import Models.Voucher;
@@ -14,10 +10,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import Interfaces.DAOs.IVoucherDAO;
 
-/**
- *
- * @author Admin
- */
 public class VoucherDAO implements IVoucherDAO {
 
     private static Connection conn = null;
