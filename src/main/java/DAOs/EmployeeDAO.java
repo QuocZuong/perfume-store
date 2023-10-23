ad2b1753c0ee12cc59b7fa25c86d406ac9428be5
phenchua29 <phenchua99122@gmail.com>
2023-10-24 02:06:19 +0700

    Merge conflict

--- src/main/java/DAOs/EmployeeDAO.java
+++ src/main/java/DAOs/EmployeeDAO.java
@@ -117,6 +117,7 @@ public class EmployeeDAO extends UserDAO implements IEmployeeDAO {
      * @param rs get a ResultSet.
      * @return return the employee that contain user information and employee
      *         information.
+     *         information.
      * @throws SQLException return error when execute SQL occur.
      */
     private Employee generateEmployeeByResultSet(ResultSet rs) throws SQLException {
@@ -368,6 +369,11 @@ public class EmployeeDAO extends UserDAO implements IEmployeeDAO {
             ps.setString(7, employee.getPhoneNumber());
             ps.setNString(8, employee.getAddress());
             ps.setLong(9, employee.getJoinDate());
+            if (employee.getRetireDate() == null) {
+                ps.setNull(10, java.sql.Types.BIGINT);
+            } else {
+                ps.setLong(10, employee.getRetireDate());
+            }
             if (employee.getRetireDate() == null) {
                 ps.setNull(10, java.sql.Types.BIGINT);
             } else {
