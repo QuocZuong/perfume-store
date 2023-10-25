c2e40a2b60051cfcbeaea109e8d5c4ff943df43b
phenchua29 <phenchua99122@gmail.com>
2023-10-25 20:18:05 +0700

    Update prevention system

--- src/main/webapp/RESOURCES/orderManager/order/public/js/list.js
+++ src/main/webapp/RESOURCES/orderManager/order/public/js/list.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const disabledBtn = document.querySelectorAll(".btn-disabled");
+  const disabledBtn = document.querySelectorAll(".btn[tabindex='-1']");
 
   for (let i = 0; i < disabledBtn.length; i++) {
     const btn = disabledBtn[i];
@@ -8,6 +8,8 @@ $(document).ready(function () {
       e.preventDefault();
       return false;
     });
+
+    btn.style.pointerEvents = "none";
 
   }
 });
