d635ddc98df71b5c075d414664e36e48e436e441
lebathinh2003 <thefanofanimeromanceandcomedy@gmail.com>
2023-10-29 15:12:08 +0700

    create test case

--- src/test/java/DAOs/UserDAOTest.java
+++ src/test/java/DAOs/UserDAOTest.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
@@ -44,9 +44,9 @@ public class UserDAOTest {
     @org.junit.jupiter.api.Test
     public void testUTCID01LoginByEmail() throws Exception {
         System.out.println("testUTCID01LoginByEmail");
-        String loginString = "test12345";
+        String loginString = "test@email.com";
         String password = "test123456789";
-        IUserDAO.loginType Type = IUserDAO.loginType.Username;
+        IUserDAO.loginType Type = IUserDAO.loginType.Email;
         UserDAO instance = new UserDAO();
         User expResult = new User();
 
@@ -69,45 +69,47 @@ public class UserDAOTest {
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID02LoginWrongPassword() throws Exception {
-        System.out.println("testUTCID02LoginWrongPassword");
-        String loginString = "test12345";
-        String password = "12345"; // Wrong password
+    public void testUTCID02LoginByEmailInvalidInput() throws Exception {
+        System.out.println("testUTCID02LoginByEmailInvalidInput");
+        String loginString = "test@email.com";
+        String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Username;
         UserDAO instance = new UserDAO();
-        assertThrows(WrongPasswordException.class, () -> {
+        assertThrows(InvalidInputException.class, () -> {
             instance.login(loginString, password, Type);
         });
     }
 
     /**
-     * UTCID03 Password null
+     * UTCID03 type null
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID03LoginPasswordNull() throws Exception {
-        System.out.println("testUTCID03LoginEmailInvalid");
-        String loginString = "test@gmail"; // invalid email
-        String password = null;
-        IUserDAO.loginType Type = IUserDAO.loginType.Email;
+    public void testUTCID03LoginByEmailTypeNull() throws Exception {
+        System.out.println("testUTCID03LoginByEmailTypeNull");
+        String loginString = "test@email.com";
+        String password = "test123456789";
+        IUserDAO.loginType Type = null;
         UserDAO instance = new UserDAO();
         assertNull(instance.login(loginString, password, Type));
     }
 
     /**
-     * UTCID04 LoginType null
+     * UTCID04 wrong password
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID04LoginLoginTypeNulll() throws Exception {
-        System.out.println("testUTCID03LoginEmailInvalid");
-        String loginString = "test@gmail"; // invalid email
-        String password = null;
+    public void testUTCID04LoginByEmailWrongPassword() throws Exception {
+        System.out.println("testUTCID04LoginByEmailWrongPassword");
+        String loginString = "test@email.com";
+        String password = "thinhthinh";
         IUserDAO.loginType Type = IUserDAO.loginType.Email;
         UserDAO instance = new UserDAO();
-        assertNull(instance.login(loginString, password, Type));
+        assertThrows(WrongPasswordException.class, () -> {
+            instance.login(loginString, password, Type);
+        });
     }
 
     /**
@@ -116,9 +118,58 @@ public class UserDAOTest {
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID05LoginEmailInvalid() throws Exception {
-        System.out.println("testUTCID03LoginEmailInvalid");
-        String loginString = "test@gmail"; // invalid email
+    public void testUTCID05LoginByEmailWrongPasswordTypeNull() throws Exception {
+        System.out.println("testUTCID04LoginByEmailWrongPasswordTypeNull");
+        String loginString = "test@email.com"; // invalid email
+        String password = "thinhthinh";
+        IUserDAO.loginType Type = null;
+        UserDAO instance = new UserDAO();
+        assertNull(instance.login(loginString, password, Type));
+    }
+
+    /**
+     * UTCID06
+     *
+     *
+     * @throws Exception
+     */
+    @org.junit.jupiter.api.Test
+    public void testUTCID06LoginByEmailPasswordNull() throws Exception {
+        System.out.println("testUTCID06LoginByEmailPasswordNull");
+        String loginString = "test@email.com";
+        String password = null;
+        IUserDAO.loginType Type = null;
+        UserDAO instance = new UserDAO();
+        assertNull(instance.login(loginString, password, Type));
+
+    }
+
+    /**
+     * UTCID07
+     *
+     * @throws Exception
+     */
+    @org.junit.jupiter.api.Test
+    public void testUTCID07LoginByEmailPassword0char() throws Exception {
+        System.out.println("testUTCID07LoginByEmailPassword0char");
+        String loginString = "test@email.com";
+        String password = "";
+        IUserDAO.loginType Type = IUserDAO.loginType.Email;
+        UserDAO instance = new UserDAO();
+        assertThrows(InvalidInputException.class, () -> {
+            instance.login(loginString, password, Type);
+        });
+    }
+
+    /**
+     * UTCID08
+     *
+     * @throws Exception
+     */
+    @org.junit.jupiter.api.Test
+    public void testUTCID08LoginByInvalidEmail() throws Exception {
+        System.out.println("testUTCID08LoginByInvalidEmail");
+        String loginString = "test@gmail";
         String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Email;
         UserDAO instance = new UserDAO();
@@ -128,31 +179,32 @@ public class UserDAOTest {
     }
 
     /**
-     * UTCID06 Boundary for login string fail case, loginString length is 50
+     * UTCID09 51 character email
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID06LoginLongLoginString() throws Exception {
-        System.out.println("testUTCID04LoginLongLoginString");
-        String loginString = "1234567890123456789012345678901234567890@gmail.com"; //long email
+    public void testUTCID09LoginBy51LongEmail() throws Exception {
+        System.out.println("testUTCID09LoginBy51LongEmail");
+        String loginString = "12345678901234567890123456789012345678901@gmail.com";
         String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Email;
         UserDAO instance = new UserDAO();
+
         assertThrows(InvalidInputException.class, () -> {
             instance.login(loginString, password, Type);
         });
     }
 
     /**
-     * UTCID07 Boundary for login string accept case, loginString length is 49
+     * UTCID10 50 character email
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID07LoginLongLoginString() throws Exception {
-        System.out.println("testUTCID05LoginLongLoginString");
-        String loginString = "123456789012345678901234567890123456789@gmail.com";
+    public void testUTCID10LoginBy50LongEmail() throws Exception {
+        System.out.println("testUTCID10LoginBy50LongEmail");
+        String loginString = "1234567890123456789012345678901234567890@gmail.com";
         String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Email;
         UserDAO instance = new UserDAO();
@@ -162,17 +214,18 @@ public class UserDAOTest {
     }
 
     /**
-     * UTCID08 login method Username
+     * UTCID11 login by username
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID08LoginByUsername() throws Exception {
-        System.out.println("testUTCID06LoginByUsername");
+    public void testUTCID11LoginByUsername() throws Exception {
+        System.out.println("testUTCID11LoginByUsername");
         String loginString = "test12345";
         String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Username;
         UserDAO instance = new UserDAO();
+
         User expResult = new User();
 
         expResult.setName("Test");
@@ -189,15 +242,15 @@ public class UserDAOTest {
     }
 
     /**
-     * UTCID09 login method Username
+     * UTCID12 login String is digit Username
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID09LoginByUsername() throws Exception {
-        System.out.println("testUTCID06LoginByUsername");
-        String loginString = "test12345";
-        String password = "12345";
+    public void testUTCID12LoginByDigitUsername() throws Exception {
+        System.out.println("testUTCID12LoginByDigitUsername");
+        String loginString = "99999999";
+        String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Username;
         UserDAO instance = new UserDAO();
 
@@ -207,30 +260,48 @@ public class UserDAOTest {
     }
 
     /**
-     * UTCID10 login String is null, Abnormal case
+     * UTCID13 login String is character Username
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID10LoginLoginStringIsNull() throws Exception {
-        System.out.println("testUTCID08LoginLoginStringIsNull");
+    public void testUTCID13LoginByCharacterUsername() throws Exception {
+        System.out.println("testUTCID13LoginByCharacterUsername");
+        String loginString = "AAAAAAAA";
+        String password = "test123456789";
+        IUserDAO.loginType Type = IUserDAO.loginType.Username;
+        UserDAO instance = new UserDAO();
+
+        assertThrows(WrongPasswordException.class, () -> {
+            instance.login(loginString, password, Type);
+        });
+    }
+
+    /**
+     * UTCID14 login String is null Username
+     *
+     * @throws Exception
+     */
+    @org.junit.jupiter.api.Test
+    public void testUTCID14LoginByUsernameNull() throws Exception {
+        System.out.println("testUTCID11LoginByUsername");
         String loginString = null;
-        String password = "12345";
+        String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Username;
         UserDAO instance = new UserDAO();
-
         assertNull(instance.login(loginString, password, Type));
+
     }
 
     /**
-     * UTCID11 login String is not character or number, Abnormal case
+     * UTCID15 login String is UTF-8 Username
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID11LoginLoginStringNotCharacterOrNumber() throws Exception {
-        System.out.println("testUTCID09LoginLoginStringNotCharacterOrNumber");
-        String loginString = "test123+-*/";
+    public void testUTCID15LoginByUTF8UsernameNull() throws Exception {
+        System.out.println("testUTCID15LoginByUTF8UsernameNull");
+        String loginString = "わたしわきれいです";
         String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Username;
         UserDAO instance = new UserDAO();
@@ -241,72 +312,53 @@ public class UserDAOTest {
     }
 
     /**
-     * UTCID12 login String is Username, length = 50
+     * UTCID16
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID12LoginByUsername() throws Exception {
-        System.out.println("testUTCID10LoginByUsername");
-        String loginString = "oneoftheesiestexampleisoneofthemostpowerfultestcas";
+    public void testUTCID16LoginByUsername0char() throws Exception {
+        System.out.println("testUTCID16LoginByUsername0char");
+        String loginString = "";
         String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Username;
         UserDAO instance = new UserDAO();
-
         assertThrows(InvalidInputException.class, () -> {
             instance.login(loginString, password, Type);
         });
     }
 
     /**
-     * UTCID13 login String is Username, length = 49
+     * UTCID17 1 character email
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID13LoginByUsername() throws Exception {
-        System.out.println("testUTCID11LoginByUsername");
-        String loginString = "oneoftheesiestexampleisoneofthemostpowerfultestca";
+    public void testUTCID17LoginBy1LongUsernamel() throws Exception {
+        System.out.println("testUTCID17LoginBy1LongUsernamel");
+        String loginString = "1";
         String password = "test123456789";
         IUserDAO.loginType Type = IUserDAO.loginType.Username;
         UserDAO instance = new UserDAO();
-
         assertThrows(WrongPasswordException.class, () -> {
             instance.login(loginString, password, Type);
         });
     }
 
     /**
-     * UTCID14 Account is deactivated
+     * UTCID18 1 login by deactivate email
      *
      * @throws Exception
      */
     @org.junit.jupiter.api.Test
-    public void testUTCID14LoginDeactivatedAccount() throws Exception {
-        System.out.println("testUTCID11LoginByUsername");
+    public void testUTCID18LoginByDeactivateEmail() throws Exception {
+        System.out.println("testUTCID18LoginByDeactivateEmail");
         String loginString = "thinhthinh123123@gmail.com";
         String password = "thinhthinh";
         IUserDAO.loginType Type = IUserDAO.loginType.Email;
         UserDAO instance = new UserDAO();
-
         assertThrows(AccountDeactivatedException.class, () -> {
             instance.login(loginString, password, Type);
         });
-    }
-
-    /**
-     * UTCID15 LoginType is other string, abnormal
-     *
-     * @throws Exception
-     */
-    @org.junit.jupiter.api.Test
-    public void testUTCID15LoginLoginTypeIsOther() throws Exception {
-        System.out.println("testUTCID11LoginByUsername");
-        String loginString = "thinhthinh123123@gmail.com";
-        String password = "thinhthinh";
-        IUserDAO.loginType Type = null;
-        UserDAO instance = new UserDAO();
-
-        assertNull(instance.login(loginString, password, Type));
     }
 }
