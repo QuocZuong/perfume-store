00f40bb554240de357edec1000880f6500426e79
phenchua29 <phenchua99122@gmail.com>
2023-10-30 15:21:15 +0700

    Refactor code

--- src/main/java/Controllers/OrderManagerController.java
+++ src/main/java/Controllers/OrderManagerController.java
@@ -443,6 +443,7 @@ public class OrderManagerController extends HttpServlet {
         boolean isChangedEmail = true;
         boolean isChangedPassword = true;
         boolean isChangedUsername = true;
+        
         // Username, email, phone number is unique
         try {
             if (!email.equals(user.getEmail())) {
@@ -486,12 +487,9 @@ public class OrderManagerController extends HttpServlet {
             return State.Fail.value;
         }
 
-        System.out.println("change password is " + isChangedPassword);
-        System.out.println("New password is before if: " + request.getParameter("pwdNew"));
         if (isChangedPassword && request.getParameter("pwdNew") != null
                 && !request.getParameter("pwdNew").equals("")) {
             newPassword = request.getParameter("pwdNew");
-            System.out.println("New password is before MD5: " + newPassword);
             newPassword = Converter.convertToMD5Hash(newPassword);
         } else {
             newPassword = user.getPassword();
@@ -519,9 +517,11 @@ public class OrderManagerController extends HttpServlet {
 
         // Sending mail
         boolean sendMailToggler = true;
+        
         if (sendMailToggler) {
             try {
                 EmailSender es = new EmailSender();
+
                 if (isChangedPassword) {
                     System.out.println("Detect password change");
                     System.out.println("sending mail changing password");
@@ -529,6 +529,7 @@ public class OrderManagerController extends HttpServlet {
                     es.sendEmailByThread(es.CHANGE_PASSWORD_NOTFICATION,
                             es.changePasswordNotifcation(updateUser));
                 }
+
                 if (isChangedEmail) {
                     System.out.println("Detect email change");
                     System.out.println("sending mail changing email");
@@ -536,6 +537,7 @@ public class OrderManagerController extends HttpServlet {
                     es.sendEmailByThread(es.CHANGE_EMAIL_NOTFICATION,
                             es.changeEmailNotification(email));
                 }
+                
                 if (isChangedUsername) {
                     System.out.println("Detect username change");
                     System.out.println("sending mail changing username");
@@ -549,11 +551,11 @@ public class OrderManagerController extends HttpServlet {
         }
 
         if (result < 1) {
-            System.out.println("update account with ID " + user.getId() + "failed");
+            System.out.println("update account with ID " + user.getId() + " failed");
             return State.Fail.value;
         }
 
-        System.out.println("update account with ID " + user.getId() + "successfully");
+        System.out.println("update account with ID " + user.getId() + " successfully");
         return State.Success.value;
     }
 
