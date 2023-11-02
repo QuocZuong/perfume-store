d44650afce6b12b41d378aa87affc4f36da5e9d1
phenchua29 <phenchua99122@gmail.com>
2023-11-02 10:57:47 +0700

    Update log

--- server/cleanAndBuild.sh
+++ server/cleanAndBuild.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+echo "-------------------------------"
+echo
+echo "Clean and build process started"
+echo
+echo "-------------------------------"
+
 cd ~/Project/ && git pull origin model && mvn package -DskipTests && sudo cp -rf ./target/SQLproject-1.war /opt/tomcat/updated/webapps/ROOT.war
+
+echo "---------------------------------"
+echo
+echo "Clean and build process completed"
+echo
+echo "---------------------------------"
