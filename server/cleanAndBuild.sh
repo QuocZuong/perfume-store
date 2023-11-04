9df4f47110283f67243d5a56b1e8d44bd00e251c
phenchua29 <phenchua99122@gmail.com>
2023-11-04 22:35:46 +0700

    Remove sudo

--- server/cleanAndBuild.sh
+++ server/cleanAndBuild.sh
@@ -7,7 +7,7 @@ echo "Clean and build process started"
 echo
 echo "-------------------------------"
 
-cd /home/quan99122/Project/ && git stash && git fetch origin && git pull && mvn package -DskipTests && sudo rm -rf /opt/tomcat/updated/webapps/ROOT && sudo rm -f /opt/tomcat/updated/webapps/ROOT.war && sudo cp -rf ./target/SQLproject-1.war /opt/tomcat/updated/webapps/ROOT.war
+cd /home/quan99122/Project/ && git stash && git fetch origin && git pull && mvn package -DskipTests && rm -rf /opt/tomcat/updated/webapps/ROOT && rm -f /opt/tomcat/updated/webapps/ROOT.war && cp -rf ./target/SQLproject-1.war /opt/tomcat/updated/webapps/ROOT.war
 
 echo "---------------------------------"
 echo
