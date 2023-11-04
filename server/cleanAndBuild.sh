ae093622df6f8afe57ae6097962035230f289495
phenchua29 <phenchua99122@gmail.com>
2023-11-04 22:30:28 +0700

    Update script

--- server/cleanAndBuild.sh
+++ server/cleanAndBuild.sh
@@ -6,7 +6,7 @@ echo "Clean and build process started"
 echo
 echo "-------------------------------"
 
-cd /home/quan99122/Project/ && git stash && git fetch origin && git pull && mvn package -DskipTests && rm -rf /opt/tomcat/updated/webapps/ROOT && rm -f /opt/tomcat/updated/webapps/ROOT.war && cp -rf ./target/SQLproject-1.war /opt/tomcat/updated/webapps/ROOT.war
+cd ~/Project/ && git stash && git fetch origin && git pull && mvn package -DskipTests && rm -rf /opt/tomcat/updated/webapps/ROOT && rm -f /opt/tomcat/updated/webapps/ROOT.war && cp -rf ./target/SQLproject-1.war /opt/tomcat/updated/webapps/ROOT.war
 
 echo "---------------------------------"
 echo
