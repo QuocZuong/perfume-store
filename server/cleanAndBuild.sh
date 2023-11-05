326adfd1209343c233dfbccdfeeefd9395fb08d8
phenchua29 <phenchua99122@gmail.com>
2023-11-05 19:21:10 +0700

    Update script

--- server/cleanAndBuild.sh
+++ server/cleanAndBuild.sh
@@ -7,7 +7,7 @@ echo "Clean and build process started"
 echo
 echo "-------------------------------"
 
-cd /home/quan99122/Project/ && echo "Went into the /Project" && git stash && echo "Stashed" && git fetch origin && echo "fetched" && git pull && echo "Pulled newest changes" && mvn package -DskipTests && sudo rm -rf /opt/tomcat/updated/webapps/ROOT && sudo rm -f /opt/tomcat/updated/webapps/ROOT.war && echo "Removed the old files" && sudo cp -rf ./target/SQLproject-1.war /opt/tomcat/updated/webapps/ROOT.war && echo "Done!"
+cd ~/Project/ && echo "Went into the /Project" && git stash && echo "Stashed" && git fetch origin && echo "fetched" && git pull && echo "Pulled newest changes" && mvn package -DskipTests && sudo rm -rf /opt/tomcat/updated/webapps/ROOT && sudo rm -f /opt/tomcat/updated/webapps/ROOT.war && echo "Removed the old files" && sudo cp -rf ./target/SQLproject-1.war /opt/tomcat/updated/webapps/ROOT.war && echo "Done!"
 
 echo "---------------------------------"
 echo
